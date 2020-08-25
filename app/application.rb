73b0f37b95ee4195d89198069ccd0b427f94a22c
Toni  Diep <adiep123@gmail.com>
2020-08-25 16:25:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       @@cart.each do |item|
-        resp.write "#{item}"
+        resp.write "#{item}\n"
     else
       resp.write "Path Not Found"
     end
