3a42b061462bacef498d63b72fdf6d5729489694
Toni  Diep <adiep123@gmail.com>
2020-08-25 16:49:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,6 +24,7 @@ class Application
     elsif req.path.match(/add/)
       add_item = req.params["item"]
       if @@items.include?(add_item)
+        @@cart << add_item
         resp.write "added #{add_item}"
       else 
         resp.write "We don't have that item"
